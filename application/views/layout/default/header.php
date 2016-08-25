fcefb562f2ff415456c4035b7b7ad25f98fc8e89
djalmocruzjr <djalmo.cruz@gmail.com>
2016-08-25 19:59:03 -0300

    Adicionando favicon para as paginas de documentacao

---
+++ application/views/layout/default/header.php
@@ -0,0 +1,34 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="API de serviços do Ambiente Virtual de Aprendizagem Móvel (AVAM) Biblivirti">
+    <meta name="keywords" content="API, JSON, Webservices, Biblivirti, Sysmob, Docs">
+    <meta name="author" content="Djalmocruzjr (djalmo.cruz@gmail.com)">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="<?php echo base_url(MEDIA_IMAGES_PATH.'favicon_green.ico');?>" />
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('vendor/twbs/bootstrap/dist/css/bootstrap.min.css'); ?>">
+    <!-- JQuery Javascript -->
+    <script href="<?php echo base_url('vendor/components/jquery/jquery.min.js'); ?>"></script>
+    <!-- Bootstrap Javascript -->
+    <script href="<?php echo base_url('vendor/twbs/bootstrap/dist/css/bootstrap.min.js'); ?>"></script>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <title><?php echo $page_title ?></title>
+</head>
+<body>
+<div class="container">
+    
+</div>
+</body>
+</html>
